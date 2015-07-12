bc8d4b38588477a4cde7f09c896b046eb93267f1
Florian Lindner <florian.lindner@xgm.de>
2015-07-12 14:14:33 +0200

    Add test main init file.
    Currently still defunct.

---
+++ src/drivers/test_main.cpp
@@ -0,0 +1,11 @@
+// #define BOOST_TEST_MAIN
+
+#include <boost/test/unit_test.hpp>
+
+#include <iostream>
+
+// bool init_unit_test()
+// {
+//   std::cout << "TEST !!!" << std::endl;
+//   return false;
+// }
